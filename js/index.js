76b463dd11bbf1a6d2b7305398f868118fc523a4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-02-28 00:29:36 +0900

    feat: 기능 개발

--- js/index.js
+++ js/index.js
@@ -17,3 +17,47 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+const commentInput = document.getElementById("commentInput");
+const commentButton = document.getElementById("commentButton");
+const commentList = document.querySelector(".comment-list");
+
+commentButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const comment = commentInput.value;
+    if (comment) {
+      const li = document.createElement("li");
+      
+      const commentItem = document.createElement("div");
+      commentItem.classList.add("comment-item");
+      
+      const commentAuthor = document.createElement("div");
+      commentAuthor.classList.add("comment-author");
+      
+      const image = document.createElement("img");
+      image.src = "./images/comment-author-icon.png"
+      image.alt = "사용자 프로필 이미지";
+      
+      const authorName = document.createElement("span");
+      authorName.textContent = "재오";
+      
+      commentAuthor.appendChild(image);
+      commentAuthor.appendChild(authorName);
+      
+      const commentContent = document.createElement("div");
+      commentContent.classList.add("comment-content");
+      commentContent.textContent = comment;
+
+      commentItem.appendChild(commentAuthor);
+      commentItem.appendChild(commentContent);
+
+      li.appendChild(commentItem);
+
+      commentList.appendChild(li);
+      commentInput.value = "";
+
+      alert("댓글이 등록되었습니다.");
+    }
+});
+  
